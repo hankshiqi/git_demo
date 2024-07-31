248f4a57fddd861ae91aacfaf74ac71ced97e613
hankyang <1951394181@qq.com>
2024-07-31 14:43:24 +0800

    first commit

---
+++ src/test/java/com/hank/learngit/LearngitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hank.learngit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearngitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
